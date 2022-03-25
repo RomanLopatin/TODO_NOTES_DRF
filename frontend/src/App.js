ba8f1bb4d0202694b9018b6826723726f7e7d717
ROL <romanlopatin@gmail.com>
2022-03-25 22:58:31 +0300

    hw-7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import LoginForm from './components/LoginForm.js';
 import UserList from './components/UserList.js'
 import ProjectList from './components/ProjectList.js'
 import ProjectTaskList from './components/ProjectTaskList.js'
@@ -18,43 +19,98 @@ class App extends React.Component{
             users:[],
             projects:[],
             tasks:[],
+            token: '',
         }
     }
 
+     is_authenticated() {
+        return !!this.state.token
+    }
+
+    logout() {
+        localStorage.setItem('token', '')
+        this.setState(
+            {
+                'token': '',
+                users:[],
+                projects:[],
+                tasks:[],
+            }, this.getData
+        )
+    }
+
+    getHeaders() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_authenticated()) {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+
+    getToken(username, password) {
+        console.log(username, password)
+        axios
+            .post('http://127.0.0.1:8000/api-auth-token/', {username: username, password: password})
+            .then(response => {
+                const token = response.data.token
+                console.log(token)
+                localStorage.setItem('token', token)
+                this.setState(
+                    {
+                        'token': token
+                    }, this.getData)
+            })
+            .catch(error => alert('Неверный логин или пароль'))
+    }
+
+     getData() {
+
+         let headers = this.getHeaders()
+
+         axios
+            .get('http://127.0.0.1:8000/api/users/',{headers})
+            .then (response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            })
+            .catch (error => console.log(error))
+        axios
+            .get('http://127.0.0.1:8000/api/projects/', {headers})
+            .then (response => {
+                const projects = response.data
+                this.setState(
+                    {
+                        'projects': projects
+                    }
+                )
+            })
+            .catch (error => console.log(error))
+        axios
+            .get('http://127.0.0.1:8000/api/tasks/', {headers})
+            .then (response => {
+                const tasks = response.data
+                this.setState(
+                    {
+                        'tasks': tasks
+                    }
+                )
+            })
+            .catch (error => console.log(error))
+    }
+
     componentDidMount() {
-    axios
-        .get('http://127.0.0.1:8000/api/users/')
-        .then (response => {
-            const users = response.data
-            this.setState(
-                {
-                    'users': users
-                }
-            )
-        })
-        .catch (error => console.log(error))
-    axios
-        .get('http://127.0.0.1:8000/api/projects/')
-        .then (response => {
-            const projects = response.data
-            this.setState(
-                {
-                    'projects': projects
-                }
-            )
-        })
-        .catch (error => console.log(error))
-    axios
-        .get('http://127.0.0.1:8000/api/tasks/')
-        .then (response => {
-            const tasks = response.data
-            this.setState(
-                {
-                    'tasks': tasks
-                }
-            )
-        })
-        .catch (error => console.log(error))
+        let token = localStorage.getItem('token')
+        this.setState(
+            {
+                'token': token
+            }, this.getData
+        )
     }
 
     render () {
@@ -62,6 +118,9 @@ class App extends React.Component{
             <BrowserRouter>
                     <nav>
                         <ul>
+                            <li>
+                                {this.is_authenticated() ? <button onClick={()=>this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
+                            </li>
                             <li><Link to='/users'>Users</Link></li>
                             <li><Link to='/projects'>Projects</Link></li>
                             <li><Link to='/tasks'>Tasks</Link></li>
@@ -69,6 +128,7 @@ class App extends React.Component{
                     </nav>
                     <Routes>
                         <Route exact path = '/' element = {<Navigate to='/users'/>}/>
+                        <Route exact path = '/login' element = {<LoginForm getToken={(username, password) => this.getToken(username, password)}/>}/>
                         <Route exact path = '/users' element = {<UserList users={this.state.users} />}/>
                         <Route exact path = '/projects' element = {<ProjectList projects={this.state.projects} />} />
                         <Route path = '/projects/project/:id' element = {<ProjectTaskList tasks={this.state.tasks}/>} />
